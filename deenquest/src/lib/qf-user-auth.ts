d9c011fde9c980a3e1b9ee83310b6a355d637b5f
AuvroIslam <crossgame2002@gmail.com>
2026-04-18 18:31:04 +0600

    fixed oAuth

--- deenquest/src/lib/qf-user-auth.ts
+++ deenquest/src/lib/qf-user-auth.ts
@@ -70,7 +70,7 @@ export async function initiateQFOAuth(): Promise<void> {
     response_type: "code",
     client_id: clientId,
     redirect_uri: redirectUri,
-    scope: "openid offline_access user collection",
+    scope: "openid offline_access bookmark.read bookmark.create bookmark.delete",
     state,
     nonce,
     code_challenge: challenge,

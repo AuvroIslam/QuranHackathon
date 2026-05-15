3ac18adb11bd3240d7ef7112cf123e75351aafd2
AuvroIslam <crossgame2002@gmail.com>
2026-05-15 18:19:08 +0600

    fixed test cases

--- deenquest/jest.config.js
+++ deenquest/jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "jest";
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleNameMapper: {
@@ -11,5 +10,3 @@ const config: Config = {
   },
   testMatch: ["**/__tests__/**/*.test.ts"],
 };
-
-export default config;
